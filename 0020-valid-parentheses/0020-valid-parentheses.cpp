fdd56636e449926c4a06389e3ec999d6ba32ae4d
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-03 22:14:31 +0530

    Time: 5 ms (1.08%), Space: 10.3 MB (6.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,29 +1,21 @@
 class Solution
 {
-    private:
-        bool isOpening(char & ch)
-        {
-            if (ch == '(' || ch == '[' || ch == '{') return true;
-            return false;
-        }
-    bool isMatched(char &top, char &current)
-    {
-        if (top == '(' && current == ')') return true;
-        if (top == '[' && current == ']') return true;
-        if (top == '{' && current == '}') return true;
-        return false;
-    }
+    unordered_map<char,int> mapping= {
+        {'(',1},{')',-1},
+        {'[',2},{']',-2},
+        {'{',3},{'}',-3},
+    };
     public:
         bool isValid(string s)
         {
             stack<char> st;
             for (int i = 0; i < s.size(); i++)
             {
-                if (isOpening(s[i])) st.push(s[i]);
+                if (mapping[s[i]]>0) st.push(mapping[s[i]]);
                 else
                 {
                     if (st.size() == 0) return false;
-                    else if (!isMatched(st.top(), s[i])) return false;
+                    else if (st.top()+mapping[s[i]]!=0) return false;
                     st.pop();
                 }
             }
