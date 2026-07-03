b48f1250de291f24e349f38ad80ba107e3553b1c
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-03 12:59:30 +0530

    Time: 0 ms (100%), Space: 10 MB (38.07%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,29 +1,75 @@
+class Node{
+public:
+    int value;
+    Node* next;
+    Node(int value){
+        this->value=value;
+        this->next=NULL;
+    }
+};
+
+
+class LinkedList {
+private:
+    Node* head;
+    int size;
+public:
+    LinkedList(){
+        this->head=NULL;
+        this->size=0;
+    }
+
+    void addNode(int value){
+        Node* newNode = new Node(value);
+        newNode->next=head;
+        head=newNode;
+        size++;
+    }
+
+    int deleteNode(){
+        if(empty())return -1;
+        Node* temp = head;
+        int poppedValue=temp->value;
+        head=head->next;
+        delete temp;
+        size--;
+        return poppedValue;
+    }
+
+    int topNode(){
+        if(empty())return -1;
+        return head->value;
+    }
+
+    bool empty(){
+        return size==0;
+    }
+
+};
+
+
 class MyStack {
 private:
-    vector<int>arr;
+    LinkedList list;
 public:
     MyStack() {
         
     }
     
     void push(int x) {
-        arr.push_back(x);
+        list.addNode(x);
     }
     
     int pop() {
-        if(empty())return -1;
-        int topElement = top();
-        arr.pop_back();
-        return topElement;
+        return list.deleteNode();
     }
     
     int top() {
-        if(empty())return -1;
-        return arr[arr.size()-1];
+        return list.topNode();
     }
     
     bool empty() {
-        return arr.size()==0;
+        return list.empty();
     }
 };
 
