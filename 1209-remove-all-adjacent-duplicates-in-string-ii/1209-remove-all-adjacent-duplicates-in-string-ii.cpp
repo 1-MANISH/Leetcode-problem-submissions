16158012fc7a088ebf457871ccb10438ffc21148
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-08 23:46:27 +0530

    Time: 16 ms (21.04%), Space: 18.1 MB (7.88%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char, int>> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (st.empty())
+                st.push({s[i], 1});
+            else {
+                if (st.top().first == s[i])
+                    st.push({s[i], st.top().second + 1});
+                else
+                    st.push({s[i], 1});
+            }
+            if (st.top().second == k) {
+                int cnt = k;
+                while (cnt--) {
+                    st.pop();
+                }
+            }
+        }
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top().first;
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
