3a4219ddc08a6de9c0c2e21962b525f6e8169752
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-14 22:39:49 +0530

    Time: 103 ms (10.37%), Space: 157.7 MB (8.46%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,54 @@
-class Solution
-{
-    public:
-        vector<int> maxSlidingWindow(vector<int> &nums, int k)
-        {
-            int n = nums.size();
-            vector<int>output;
-            priority_queue<pair<int,int>> maxHeap;
-            // first window
-            for (int i = 0; i < k; i++) {
-                maxHeap.push({nums[i],i});
-            }
-            output.push_back(maxHeap.top().first);
-            // further windows
-            for(int i = k ; i< n ; i++){
-                if(maxHeap.top().second<=i-k){
-                    while(!maxHeap.empty() && maxHeap.top().second<=i-k)maxHeap.pop();
+class MyAggregateQueue {
+public:
+    stack<pair<int, int>> inStack, outStack;
+    MyAggregateQueue() {}
+
+    // O(1)
+    void push(int x) {
+        if (inStack.empty())
+            inStack.push({x, x});
+        else
+            inStack.push({x, max(x, inStack.top().second)});
+    }
+
+    // Amortized - O(1)
+    void pop() {
+        if (outStack.empty()) {
+            while (!inStack.empty()) {
+                if (outStack.empty()) {
+                    outStack.push({inStack.top().first,inStack.top().first});
+                } else {
+                    outStack.push({inStack.top().first,max(inStack.top().first, outStack.top().second)});
                 }
-                maxHeap.push({nums[i],i});
-                output.push_back(maxHeap.top().first);
+                inStack.pop();
             }
-            return output;
         }
+        outStack.pop();
+    }
+
+    int getMax() {
+        if (inStack.empty())
+            return outStack.top().second;
+        if (outStack.empty())
+            return inStack.top().second;
+        return max(inStack.top().second, outStack.top().second);
+    }
+};
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> output;
+        MyAggregateQueue sQueue;
+        for (int i = 0; i < k; i++) {
+            sQueue.push(nums[i]);
+        }
+        output.push_back(sQueue.getMax());
+        for (int i = k; i < nums.size(); i++) {
+            sQueue.push(nums[i]);
+            sQueue.pop();
+            output.push_back(sQueue.getMax());
+        }
+        return output;
+    }
 };
