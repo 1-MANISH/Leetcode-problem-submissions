7dac452d3b454249775014f7259df9d39a11699e
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-15 13:37:35 +0530

    Time: 55 ms (18.69%), Space: 153.3 MB (13.76%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,53 +1,17 @@
-class MyAggregateQueue {
-public:
-    stack<pair<int, int>> inStack, outStack;
-    MyAggregateQueue() {}
-
-    // O(1)
-    void push(int x) {
-        if (inStack.empty())
-            inStack.push({x, x});
-        else
-            inStack.push({x, max(x, inStack.top().second)});
-    }
-
-    // Amortized - O(1)
-    void pop() {
-        if (outStack.empty()) {
-            while (!inStack.empty()) {
-                if (outStack.empty()) {
-                    outStack.push({inStack.top().first,inStack.top().first});
-                } else {
-                    outStack.push({inStack.top().first,max(inStack.top().first, outStack.top().second)});
-                }
-                inStack.pop();
-            }
-        }
-        outStack.pop();
-    }
-
-    int getMax() {
-        if (inStack.empty())
-            return outStack.top().second;
-        if (outStack.empty())
-            return inStack.top().second;
-        return max(inStack.top().second, outStack.top().second);
-    }
-};
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> output;
-        MyAggregateQueue sQueue;
-        for (int i = 0; i < k; i++) {
-            sQueue.push(nums[i]);
+        vector<int>output;
+        priority_queue<pair<int,int>>maxHeap;
+        for(int i = 0 ; i< k ; i++){
+            maxHeap.push({nums[i],i});
         }
-        output.push_back(sQueue.getMax());
-        for (int i = k; i < nums.size(); i++) {
-            sQueue.push(nums[i]);
-            sQueue.pop();
-            output.push_back(sQueue.getMax());
+        output.push_back(maxHeap.top().first);
+        for(int i = k ; i< nums.size() ; i++){
+            // remove index<i-k+1;
+            while(!maxHeap.empty() && maxHeap.top().second<i-k+1)maxHeap.pop();
+            maxHeap.push({nums[i],i});
+            output.push_back(maxHeap.top().first);
         }
         return output;
     }
