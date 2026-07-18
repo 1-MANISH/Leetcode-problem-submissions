fbe259fb4685935d13c726931a76c06cb688ac55
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-18 17:07:37 +0530

    Time: 0 ms (100%), Space: 21.4 MB (96.52%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -6,22 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isSum(TreeNode* root , int currentSum,int &targetSum){
-        if(root==NULL)return false;
-        currentSum+=root->val;
-        if(targetSum==currentSum && root->left==NULL && root->right==NULL)return true;
-        bool leftCall = isSum(root->left,currentSum,targetSum);
-        bool rightCall = isSum(root->right,currentSum,targetSum);
+    bool isSum(TreeNode* root, int targetSum) {
+        if (root == NULL)
+            return false;
+        targetSum = targetSum - root->val;
+        if (targetSum == 0 && root->left == NULL && root->right == NULL)
+            return true;
+        bool leftCall = isSum(root->left, targetSum);
+        bool rightCall = isSum(root->right, targetSum);
 
         return (leftCall || rightCall);
-
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return isSum(root,0,targetSum);
+        return isSum(root, targetSum);
     }
 };
