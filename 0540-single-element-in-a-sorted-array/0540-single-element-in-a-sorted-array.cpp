040bc5445000d62471c945a9c821591f01374770
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-20 14:29:04 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -8,17 +8,17 @@ public:
 
             if(sideElements & 1){
                 // odd elements in side
-                if(arr[mid]==arr[mid-1])
+                if(arr[mid]==arr[mid-1]) // right me avaible
                     start=mid+1;
-                else if(arr[mid]==arr[mid+1])
+                else if(arr[mid]==arr[mid+1]) // left me available
                     end=mid-1;
                 else
                     return arr[mid];
             }else{
                 // even elements in side
-                if(arr[mid]==arr[mid-1])
+                if(arr[mid]==arr[mid-1])//left me available
                     end=mid-2;
-                else if(arr[mid]==arr[mid+1])
+                else if(arr[mid]==arr[mid+1])// right me available
                     start=mid+2;
                 else
                     return arr[mid];
@@ -28,3 +28,14 @@ public:
         return arr[start];
     }
 };
+
+/*
+n = 9
+1,1,2,3,3,4,4,8,8
+s = 0 e = 8 | m=4
+1,1,2
+s = 0 e = 2 | m=1
+s = 2 | e = 2 
+2
+return arr[start]
+*/
