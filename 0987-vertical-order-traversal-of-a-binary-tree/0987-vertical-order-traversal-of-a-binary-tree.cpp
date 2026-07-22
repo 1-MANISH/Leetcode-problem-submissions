38b3b0b8defe2820e1befcdf5c347d224600eae8
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-07-22 16:11:03 +0530

    Time: 0 ms (100%), Space: 15.6 MB (82.7%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,35 +11,28 @@
  */
 class Solution {
 public:
-
-    static bool cap(pair<int,int>&p,pair<int,int>&q){
-        if(p.first==q.first){// level same
-            return p.second<q.second;//value chota phle
-        }else return p.first<q.first;// chota phle always
-    }
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
+    vector<vector<int>> verticalTraversal(TreeNode* root) {        
         vector<vector<int>>output;
-        queue<tuple<TreeNode*,int,int>>q;//{node,dist,level}
-        unordered_map<int,vector<pair<int,int>>>mp;
-        int start = INT_MAX ,end = INT_MIN;
+        if(root==NULL)return output;
+        unordered_map<int,vector<pair<int,int>>>mp; //dist -> nodes[{level,value}]
+        queue<tuple<TreeNode*,int,int>>q; //{node,dist,level} -> horizontal distance
         q.push({root,0,0});
+        int start = INT_MAX ,  end = INT_MIN;
         while(!q.empty()){
-            auto [curr,dist,level] = q.front();
+            auto[node,dist,level] = q.front();
             q.pop();
-            mp[dist].push_back({level,curr->val});
+            auto it = mp.find(dist);
             start = min(start,dist);
             end = max(end,dist);
-            if(curr->left!=NULL)q.push({curr->left,dist-1,level+1});
-            if(curr->right!=NULL)q.push({curr->right,dist+1,level+1});
+            mp[dist].push_back({level,node->val});
+            if(node->left!=NULL)q.push({node->left,dist-1,level+1});
+            if(node->right!=NULL)q.push({node->right,dist+1,level+1});
         }
-        // sort
-        // level - alag then p first
-        // value - level equal then
-        for(int dist=start ;dist<=end;dist++){
+        for(int dist = start ;dist <=end;dist++){
             auto nodes = mp[dist];
-            sort(nodes.begin(),nodes.end(),cap);
+            sort(nodes.begin(),nodes.end());
             vector<int>temp;
-            for(auto &ele:nodes)temp.push_back(ele.second);
+            for(auto &e:nodes)temp.push_back(e.second);
             output.push_back(temp);
         }
         return output;
