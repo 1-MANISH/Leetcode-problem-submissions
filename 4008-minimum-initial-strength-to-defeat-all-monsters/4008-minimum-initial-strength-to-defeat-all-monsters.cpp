a10eb586abe00c418341e85b8661c32f3db18585
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-01 21:32:18 +0530

    Time: 47 ms (67.18%), Space: 243.4 MB (81.11%) - LeetHub

---
+++ 4008-minimum-initial-strength-to-defeat-all-monsters/4008-minimum-initial-strength-to-defeat-all-monsters.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+     using ll = long long;
+
+    bool possible(ll initial, vector<int>& monsters, vector<ll>& bonus) {
+        ll strength = initial;
+
+        for (int i = 0; i < monsters.size(); i++) {
+            if (strength + bonus[i] < monsters[i])
+                return false;
+
+            strength -= monsters[i];
+            if (strength < 0)
+                strength = 0;
+        }
+
+        return true;
+    }
+    long long minInitialStrength(vector<int>& monsters, vector<vector<int>>& boosts) {
+        int n = monsters.size();
+
+        //  bonuses
+        vector<ll> bonus(n + 1, 0);
+
+        for (auto &b : boosts) {
+            int l = b[0];
+            int r = b[1];
+            ll val = b[2];
+            bonus[l] += val;
+            if (r + 1 < n) // to not this bonous to index >r
+                bonus[r + 1] -= val;
+        }
+        for (int i = 1; i < n; i++) {
+            bonus[i] = bonus[i-1]+bonus[i];
+        }
+        ll low = 0,high = 0;
+        for (int x : monsters)
+            high += x;
+        while (low < high) {
+            ll mid = low + (high - low) / 2;
+            if (possible(mid, monsters, bonus))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
