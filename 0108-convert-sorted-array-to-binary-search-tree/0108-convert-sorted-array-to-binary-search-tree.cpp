22143af3cd393a51f8b706d8614c88f735204df7
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-03 21:08:16 +0530

    Time: 4 ms (34.23%), Space: 22.7 MB (97.68%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -11,18 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode* makeBalanceTree(vector<int>&nums,int s,int e){
-        if(s>e)return NULL;
+    TreeNode* helper(vector<int>&nums,int l,int r){
+        if(l>r)return NULL;
+          
+        int m =( l+r)/2;
+        TreeNode* root = new TreeNode(nums[m]);
+        root->left  = helper(nums,l,m-1);
+        root->right = helper(nums,m+1,r);
 
-        int m = s+(e-s)/2;
-        TreeNode* node = new TreeNode(nums[m]);
-
-        node->left = makeBalanceTree(nums,s,m-1);
-        node->right = makeBalanceTree(nums,m+1,e);
-
-        return node;
+        return root;
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return makeBalanceTree(nums,0,nums.size()-1);
+        return helper(nums,0,nums.size()-1);
     }
 };
