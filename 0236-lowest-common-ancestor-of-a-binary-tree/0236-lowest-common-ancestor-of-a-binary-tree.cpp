c4717f789672f391a550125f4e470ab9ae42ea99
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-11 22:40:40 +0530

    Time: 68 ms (32.57%), Space: 51.1 MB (6.24%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,30 +9,23 @@
  */
 class Solution {
 public:
-    bool isFind = false;
-    void Ancestor(TreeNode* root,TreeNode* node,vector<TreeNode*>&anc){
-        if(root==NULL){
-            return;
-        }
 
+    void Ancestor(TreeNode* root,TreeNode* node,vector<TreeNode*>&anc){
+        if(root==NULL)return;
         if(root->val==node->val){
             anc.push_back(root);
-            isFind=true;
             return;
         };
-        if(isFind)return;
         anc.push_back(root);
         Ancestor(root->left,node,anc);
-        if(isFind)return;
         Ancestor(root->right,node,anc);
-        if(isFind)return;
+        if(anc.size() && anc[anc.size()-1]->val!=node->val)
         anc.pop_back();
 
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         vector<TreeNode*>a,b;
         Ancestor(root,p,a);
-        isFind=false;
         Ancestor(root,q,b);
         reverse(a.begin() , a.end());
         reverse(b.begin() , b.end());
