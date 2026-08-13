43b4080299c1e7275f066db1213b6b5d7b2af2a6
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-13 14:24:02 +0530

    Time: 99 ms (5.2%), Space: 161.8 MB (5.03%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,35 +13,34 @@ class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
 
-        if(preorder.size()==0){
-            return NULL;
-        }
-
+        if(preorder.size()==0)return NULL;
+        
         TreeNode* root = new TreeNode(preorder[0]);
 
         int idx = -1;
-        for(int i = 0 ; i < inorder.size() ; i++){
-            if(inorder[i]==root->val){
-                idx=i;
+        for(int i = 0 ; i< inorder.size() ;i++){
+            if(inorder[i]==preorder[0]){
+                idx = i;
                 break;
             }
         }
 
-        vector<int>porder_left;//[1,idx]
-        vector<int>porder_right;//[idx+1,last last]
-        vector<int>inorder_left;//[0,idx-1]
-        vector<int>inorder_right;//[idx+1,till last]
+        vector<int>p_order1;//[1,idx]
+        vector<int>p_order2;//[idx+1,n-1];
+        vector<int>i_order1;//[0,idx-1]
+        vector<int>i_order2;//[idx+1,n-1];
 
-        for(int i  = 1 ; i <=idx ; i++)porder_left.push_back(preorder[i]);
-        for(int i  = idx+1 ; i <preorder.size() ; i++)porder_right.push_back(preorder[i]);
+        for(int i = 1 ; i<=idx;i++)p_order1.push_back(preorder[i]);
+        for(int i = idx+1;i<preorder.size();i++)p_order2.push_back(preorder[i]);
+        for(int i = 0 ; i< idx;i++)i_order1.push_back(inorder[i]);
+        for(int i = idx+1 ; i< inorder.size();i++)i_order2.push_back(inorder[i]);
 
-        for(int i  = 0 ; i <idx ; i++)inorder_left.push_back(inorder[i]);
-        for(int i  = idx+1 ; i <inorder.size() ; i++)inorder_right.push_back(inorder[i]);
+        TreeNode* left_tree = buildTree(p_order1,i_order1);
+        TreeNode* right_tree = buildTree(p_order2,i_order2);
 
-        root->left = buildTree(porder_left,inorder_left);
-        root->right = buildTree(porder_right,inorder_right);
+        root->left=left_tree;
+        root->right=right_tree;
 
         return root;
-
     }
 };
