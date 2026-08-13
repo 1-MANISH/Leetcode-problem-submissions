962c4587c1118bb95682a87145d9d1f5f541dc2c
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-13 13:04:12 +0530

    Time: 61 ms (72.24%), Space: 51 MB (7.52%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,24 +9,32 @@
  */
 class Solution {
 public:
+    
+    void findPath(TreeNode* root, TreeNode* target,vector<TreeNode*>&nodes){
+        if(root==NULL)return;
+        if(root->val==target->val){
+            nodes.push_back(root);
+            return;
+        }
+        nodes.push_back(root);
+        findPath(root->left,target,nodes);
+        findPath(root->right,target,nodes);
+        if(nodes.size() && nodes[nodes.size()-1]->val!=target->val)nodes.pop_back();
+        
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)return NULL;
-
-        if(root==p or root==q)return root;
-
-        TreeNode* left = lowestCommonAncestor(root->left,p,q);
-        TreeNode* right = lowestCommonAncestor(root->right,p,q);
-
-        // at this node (p and q find)
-        if(left!=NULL && right!=NULL)return root;
-
-        // p found
-        if(left!=NULL && right==NULL)return left;
-
-        // q found
-        if(left==NULL && right!=NULL)return right;
-
-        // nothing found
-        return NULL;
+        vector<TreeNode*>a,b;
+        findPath(root,p,a);
+        findPath(root,q,b);
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int i = a.size()-1 , j = b.size()-1 ;
+        TreeNode* ans = root;
+        while(i>=0 && j>=0){
+            if(a[i]->val==b[j]->val) ans = a[i];
+            i--;
+            j--;
+        }
+        return ans;
     }
 };
