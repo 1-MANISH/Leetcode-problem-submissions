8c1461345fd69a5212ceb66060ad8c0849117b8a
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-14 14:59:28 +0530

    Time: 4 ms (6.81%), Space: 28.3 MB (12.46%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct Data{
+    int straightPathSum=0;
+    int maxPathSum=INT_MIN;
+};
+
+class Solution {
+public:
+
+    Data maxPath(TreeNode* root){
+        if(root==NULL){
+            return Data();
+        }
+        Data leftAns = maxPath(root->left);
+        Data rightAns = maxPath(root->right);
+
+        Data ans;
+        ans.straightPathSum = max(max(leftAns.straightPathSum,rightAns.straightPathSum)+root->val ,root->val);
+        ans.maxPathSum = max(root->val,max(max(leftAns.maxPathSum,rightAns.maxPathSum),max(leftAns.straightPathSum+rightAns.straightPathSum+root->val,max(leftAns.straightPathSum,rightAns.straightPathSum)+root->val)));
+
+        return ans;
+    }
+    int maxPathSum(TreeNode* root) {
+        Data ans = maxPath(root);
+        return max(ans.straightPathSum,ans.maxPathSum);
+    }
+};
