9cc4cf334bf26d65384ceab7bb0d5501693e3dd0
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-20 15:53:40 +0530

    Time: 3 ms (50.36%), Space: 18.1 MB (31.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,13 +1,18 @@
 struct Data{
     int value,count;
 };
+struct CompareData{
+    bool operator()(const Data&a,const Data&b){
+        return a.count>b.count;
+    }
+};
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>freq;
         for(auto &num:nums)freq[num]++;
-        priority_queue<Data,vector<Data>,decltype([](const Data &a,const Data &b){return a.count>b.count;})>minHeap;
+        priority_queue<Data,vector<Data>,CompareData>minHeap;
         for(auto [value,count]:freq){
             minHeap.push({value,count});
             if(minHeap.size()>k){
