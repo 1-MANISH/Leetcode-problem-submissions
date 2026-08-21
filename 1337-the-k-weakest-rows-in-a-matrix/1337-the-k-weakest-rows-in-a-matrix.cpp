84e7f7042cf2424603b3f72a2356ead4890f3f5a
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-08-21 21:24:45 +0530

    Time: 2 ms (9.5%), Space: 14.4 MB (39.49%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,25 @@
+template <typename T>
+using MaxHeap = priority_queue<T>;
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        MaxHeap<pair<int,int>>mxHeap;//{solderCount,rowIndex};
+        vector<int>weekestRows;
+        int m = mat.size() , n =mat[0].size();
+        for(int i = 0 ;i < m ; i++){
+            int solderCount=0;
+            for(int j = 0 ; j < n ; j++)solderCount+=mat[i][j];
+            mxHeap.push({solderCount,i});
+            if(mxHeap.size()>k){
+                mxHeap.pop();
+            }
+        }
+        while(!mxHeap.empty()){
+            weekestRows.push_back(mxHeap.top().second);
+            mxHeap.pop();
+        }
+        reverse(weekestRows.begin(),weekestRows.end());
+        return weekestRows;
+    }
+};
