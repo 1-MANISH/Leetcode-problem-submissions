5ea22ae82ac27e9dbbbfd066a1f5ada50b10e467
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-09-01 17:17:35 +0530

    Time: 0 ms (100%), Space: 21.6 MB (76.55%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,80 +1,31 @@
 class Solution {
 public:
-
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        int s1 = intervals[0][0] ,  e1 = intervals[0][1] , index = 1, n = intervals.size();
+    // [ [1,3], [6,10], [16,19]]  [0,0]
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
         vector<vector<int>>output;
+        int s1 = newInterval[0] ,  e1 = newInterval[1] ,s2,e2;
+        int index=0 , overlap = 0 ;
         while(index<n){
-            int s2 = intervals[index][0] , e2 = intervals[index][1];
-            // overlap 
+            s2 = intervals[index][0] ,  e2 = intervals[index][1];
             if(max(s1,s2)<=min(e1,e2)){
-                e1 = max(e1,e2);
+                s1=min(s1,s2);
+                e1=max(e1,e2);
+                overlap=1;
             }else{
-                output.push_back({s1,e1});
-                s1=intervals[index][0];
-                e1=intervals[index][1];
+                //  overlap done - 
+                // either - no further overlap also - need to insert here
+                if((s1 < s2 && e1 < e2 && overlap!=10) || overlap==1 ){
+                    output.push_back({s1,e1});
+                    overlap=10;
+                }
+                output.push_back({s2,e2});
             }
             index++;
         }
-        output.push_back({s1,e1});
-        return output;
-    }
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>newIntervals;
-        int n = intervals.size();
-        int s1,s2,e1,e2;
-        s2=newInterval[0];
-        e2=newInterval[1];
-        for(int i = 0 ; i< n ; i++){
-            s1=intervals[i][0];
-            e1=intervals[i][1];
-            if(max(s1,s2)<=min(e1,e2)){
-                if(s1<s2){
-                    newIntervals.push_back({s1,e1});
-                    newIntervals.push_back({s2,e2});
-                }
-                else if(s1>s2){
-                    newIntervals.push_back({s2,e2});
-                    newIntervals.push_back({s1,e1});
-                }
-                else if(e1<e2){
-                    newIntervals.push_back({s1,e1});
-                    newIntervals.push_back({s2,e2});
-                }
-                else {
-                    newIntervals.push_back({s2,e2});
-                    newIntervals.push_back({s1,e1});
-                }
-                i++;
-                for(;i<n;i++)
-                    newIntervals.push_back({intervals[i][0],intervals[i][1]});
-                break;
-            }else {
-                if(i+1==n){
-                    if(s1<s2){
-                        newIntervals.push_back({s1,e1});
-                        newIntervals.push_back({s2,e2});
-                    }
-                    else if(s1>s2){
-                        newIntervals.push_back({s2,e2});
-                        newIntervals.push_back({s1,e1});
-                    }
-                    else if(e1<e2){
-                        newIntervals.push_back({s1,e1});
-                        newIntervals.push_back({s2,e2});
-                    }
-                    else {
-                        newIntervals.push_back({s2,e2});
-                        newIntervals.push_back({s1,e1});
-                    }
-                }
-                else {
-                    newIntervals.push_back({s1,e1});
-                }
-            }
+        if(n==0 || overlap!=10 ){
+            output.push_back({s1,e1});
         }
-        if(n==0)newIntervals.push_back({s2,e2});
-        return merge(newIntervals);
+        return output;
     }
 };
