ccc41066d24ed0adfd062916a3f6ff7e712b6936
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-09-19 20:27:18 +0530

    Time: 0 ms (100%), Space: 15.4 MB (83.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,16 +1,16 @@
-#define ll long long
+
 const int N = 200;
 const int M = 200;
+int dp[N + 1][M+1];
+
 class Solution {
 
-    int dp[N + 1][M+1];
-
 public:
-    ll f(int i, int j, int n, int m, vector<vector<int>>& cost) {
+    int f(int i, int j, int n, int m, vector<vector<int>>& cost) {
 
         // not possible as - index not valid
         if (i >= n or j >= m)
-            return INT_MAX;
+            return INT_MAX-200;
 
         // reached at bottom right end - a[n-1][m-1];
         if (i == n - 1 && j == m - 1)
@@ -22,19 +22,14 @@ public:
 
         // right + down
 
-        ll ans1 = cost[i][j] + f(i, j + 1, n, m, cost) ;
-        ll ans2 = cost[i][j] + f(i + 1, j, n, m, cost) ;
+        int ans1 = cost[i][j] + f(i, j + 1, n, m, cost) ;
+        int ans2 = cost[i][j] + f(i + 1, j, n, m, cost) ;
 
-        return dp[i][j] = 1LL*min(ans1,ans2);
+        return dp[i][j] = min(ans1,ans2);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                dp[i][j] = -1;
-            }
-        }
-
+        memset(dp,-1,sizeof dp);
         return f(0, 0, n, m, grid);
     }
 };
