38c6f0aa103a170910ed9ca4a45741bf3d93a5c0
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-09-19 20:11:31 +0530

    Time: 0 ms (100%), Space: 7.9 MB (37.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,15 @@
+const int N =3e1+10;
+int dp[N];
+
 class Solution {
 public:
-    int fib(int n) {
+    int fibHelper(int n) {
         if(n==0 || n==1)return n;
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1)return dp[n];
+        return dp[n] =  fibHelper(n-1)+fibHelper(n-2);
+    }
+    int fib(int n) {
+        memset(dp,-1,sizeof dp);
+        return fibHelper(n);
     }
 };
