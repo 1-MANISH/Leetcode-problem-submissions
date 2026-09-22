b4ef4668c390911b6670aa7a8de4c61a705a2735
Manish Patidar <91561321+1-MANISH@users.noreply.github.com>
2026-09-22 21:19:26 +0530

    Time: 23 ms (72.86%), Space: 12.6 MB (76.19%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,32 @@
+const int N = 1e3;
+int dp[N][N];
+
+class Solution {
+
+    int solve(int i ,int j , string &s,string &t){
+
+        // base case
+        if(j==t.size())return 1;
+
+        if(i==s.size())return 0;
+
+        if(dp[i][j]!=-1)return dp[i][j];
+        // not take
+
+        int ans1  = solve(i+1,j,s,t);
+
+        // take it
+        int ans2 = 0 ;
+
+        if(s[i]==t[j]){
+            ans2 = solve(i+1,j+1,s,t);
+        }
+
+        return dp[i][j] =ans1+ans2;
+    }
+public:
+    int numDistinct(string s, string t) {
+        memset(dp,-1,sizeof dp);
+        return solve(0,0,s,t);
+    }
+};
